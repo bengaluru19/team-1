d13ef7db68a33b2288dc518be2f4f7a065410574
Alex Denver <dnvr.dsz@gmail.com>
2019-07-06 17:31:34 +0530

    Header Added

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: "", 
+  component: HomeComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- web/src/app/header/header.component.ts
+++ web/src/app/header/header.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import {MatMenuModule, MatMenuTrigger} from '@angular/material/menu';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
 
-  constructor() { }
+export class HeaderComponent {
+  // @ViewChild(MatMenuTrigger) trigger: MatMenuTrigger;
 
-  ngOnInit() {
-  }
-
+  // someMethod() {
+  //   this.trigger.openMenu();
+  // }
 }
